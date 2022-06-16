a1fde498bfc1f32a8b5253555c9cc0a4051e7d35
florianjuesten <flojuesten@gmail.com>
2022-06-16 21:27:40 +0200

    new runner

--- runner.sh
+++ runner.sh
@@ -1,31 +1,100 @@
-#!/bin/zsh
+#!/bin/bash
 
-sleepUntil() { # args [-q] <HH[:MM[:SS]]> [more days]
-    local slp tzoff now quiet=false
-    [ "$1" = "-q" ] && shift && quiet=true
-    local -a hms=(${1//:/ })
-    printf -v now '%(%s)T' -1
-    printf -v tzoff '%(%z)T\n' $now
-    tzoff=$((0${tzoff:0:1}(3600*${tzoff:1:2}+60*${tzoff:3:2})))
-    slp=$((
-       ( 86400+(now-now%86400) + 10#$hms*3600 + 10#${hms[1]}*60 + 
-         ${hms[2]}-tzoff-now ) %86400 + ${2:-0}*86400
-    ))
-    $quiet || printf 'sleep %ss, -> %(%c)T\n' $slp $((now+slp))
-    sleep $slp
+# constants
+mon=1
+tue=2
+wed=3
+thu=4
+fri=5
+sat=6
+sun=7
+
+# settings
+rundays=($sat $sun)
+runhours=(7 8 9)
+runminutes=(0 10 20 30 40 50)
+runseconds=(0)
+
+# helper
+echo_time() {
+    echo $(date) $1
 }
+today=$(date +%Y-%m-%d)
+
+# run loop
+while true;do
+  echo_time ">>> RUNNING"
+  echo_time ">>> ..."
+
+  # get now
+  now_week_day=$(date +%u)
+  now_hour=$(date +%H)
+  now_minute=$(date +%M)
+  now_second=$(date +%S)
+
+  # calc sleeptime
+  diff_days_to_next_run=100
+  diff_hours_to_next_run=100
+  diff_minutes_to_next_run=100
+  diff_seconds_to_next_run=1000
+  for runday in ${rundays[@]}; do
+    diff_to_run_day=$((runday-now_week_day))
+    if [[ $diff_to_run_day -gt 0 && $diff_to_run_day -lt diff_days_to_next_run ]];then
+      diff_days_to_next_run=$diff_to_run_day
+    fi
+  done
+
+  if [[ $diff_days_to_next_run -gt 0 ]] ; then
+  diff_hours_to_next_run=${runhours[0]}
+  diff_minutes_to_next_run=${runminutes[0]}
+  diff_seconds_to_next_run=${runseconds[0]}
+else
 
 
-sleepTil(){
-  current_epoch=$(date +%s)
-  target_epoch=$(date -d '06/16/2022 22:00' +%s)
 
-  sleep_seconds=$(( $target_epoch - $current_epoch ))
 
-  echo $sleep_seconds
+  for runhour in ${runhours[@]}; do
+    if [[ $runhour -lt $now_hour ]] ; then
+      runhour=$((runhour+24))
+    fi
 
-}
+    diff_to_run_hour=$((runhour-now_hour))
+    if [[ $diff_to_run_hour -gt 0 && $diff_to_run_hour -lt diff_hours_to_next_run ]];then
+      diff_hours_to_next_run=$diff_to_run_hour
+    fi
+  done
+  diff_hours_to_next_run=$((diff_hours_to_next_run-1))
 
-sleepTil
+  for runminute in ${runminutes[@]}; do
+    if [[ $runminute -lt $now_minute ]] ; then
+      runminute=$((runminute+60))
+    fi
 
-# sleepUntil 20:10
+    diff_to_run_minute=$((runminute-now_minute))
+    if [[ $diff_to_run_minute -gt 0 && $diff_to_run_minute -lt diff_minutes_to_next_run ]];then
+      diff_minutes_to_next_run=$diff_to_run_minute
+    fi
+  done
+  diff_minutes_to_next_run=$((diff_minutes_to_next_run-1))
+
+  for runsecond in ${runseconds[@]}; do
+    if [[ $runsecond -lt $now_second ]] ; then
+      runsecond=$((runsecond+60))
+    fi
+
+    diff_to_run_second=$((runsecond-now_second))
+    if [[ $diff_to_run_second -gt 0 && $diff_to_run_second -lt diff_seconds_to_next_run ]];then
+      diff_seconds_to_next_run=$diff_to_run_second
+    fi
+  done
+
+  fi
+ # wait till next tun
+  echo_time ">>> $diff_days_to_next_run days to wait till next run."
+  echo_time ">>> $diff_hours_to_next_run hours to wait till next run."
+  echo_time ">>> $diff_minutes_to_next_run minutes to wait till next run."
+  echo_time ">>> $diff_seconds_to_next_run seconds to wait till next run."
+  date -d "$diff_days_to_next_run days $diff_hours_to_next_run hours $diff_minutes_to_next_run minutes $diff_to_run_second seconds"
+
+  sleep 5
+done
